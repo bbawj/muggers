3ebed572c3233583c726f9b2ce4bf3a33018fa80
unknown <brendanawjang@gmail.com>
2021-06-02 23:42:12 +0800

    added deletion cloud functions for channels and groups

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,36 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-const {db} = require("./util/admin");
-const firestore_tools = require('firebase-tools');
+const firebase_tools = require("firebase-tools");
+
+exports.deleteChannelRecursive = functions.region('asia-east2')
+                                .firestore.document('/groups/{groupId}/channels/{channelId}')
+                                .onDelete( async (snapshot, context) => {
+                                    const groupId = context.params.groupId
+                                    const channelId = context.params.channelId
+                                    const path = `/groups/${groupId}/channels/${channelId}`
+                                    await firebase_tools.firestore.delete(path, {
+                                        project: process.env.GCP_PROJECT,
+                                        recursive: true,
+                                        yes: true
+                                    })
+                                    return {
+                                        path: path
+                                    }
+                                })
+
+exports.deleteGroupRecursive = functions.region('asia-east2')
+                                .firestore.document('/groups/{groupId}')
+                                .onDelete( async (snapshot, context) => {
+                                    const groupId = context.params.groupId
+                                    const path = `/groups/${groupId}`
+                                    await firebase_tools.firestore.delete(path, {
+                                        project: process.env.GCP_PROJECT,
+                                        recursive: true,
+                                        yes: true
+                                    })
+                                    return {
+                                        path: path
+                                    }
+                                })
+                                
